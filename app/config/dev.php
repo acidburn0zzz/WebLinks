94475b6c03d8c382fb1165c1b19d4d9df4fd1e4d
Stéphane Bullier <manix.thedevelopper@gmail.com>
2016-04-10 21:46:09 +0200

    Mise en place test fonctionnels

--- app/config/dev.php
+++ app/config/dev.php
@@ -1,7 +1,15 @@
 <?php
 
-// include the prod configuration
-require __DIR__.'/prod.php';
+// Doctrine (db)
+$app['db.options'] = array(
+  'driver'   => 'pdo_mysql',
+  'charset'  => 'utf8',
+  'host'     => '127.0.0.1',  // Mandatory for PHPUnit testing
+  'port'     => '3306',
+  'dbname'   => 'weblinks',
+  'user'     => 'weblinks_user',
+  'password' => 'secret'
+);
 
 // enable the debug mode
 $app['debug'] = true;
